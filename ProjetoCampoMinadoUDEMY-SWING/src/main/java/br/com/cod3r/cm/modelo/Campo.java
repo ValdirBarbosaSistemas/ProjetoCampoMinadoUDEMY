e4095d746aa252aa851e11b8ac740965f574c721
Valdir Barbosa da Silva Junior <valdir.barbosasistemas@gmail.com>
2020-06-20 16:02:49 -0300

    Novo Projeto CampoMinado com SWING
    Interface gráfica com SWING

---
+++ ProjetoCampoMinadoUDEMY-SWING/src/main/java/br/com/cod3r/cm/modelo/Campo.java
@@ -0,0 +1,132 @@
+package br.com.cod3r.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Campo {
+
+    private final int linha;
+    private final int coluna;
+    private boolean aberto; // Por padrão ele vai receber falso
+    private boolean minado; // Por padrão ele vai receber falso
+    private boolean marcado; // Por padrão ele vai receber falso
+
+    // Criado uma lista do tipo campo para ser adicionado os elementos
+    private List<Campo> vizinhos = new ArrayList<Campo>();
+
+    // Construtor
+    Campo(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    // Métodos
+    boolean adicionarVizinho(Campo vizinho) {
+        /*
+		 * Para calcular nessa fase tem que ter em mente que para calcular a coluna, ela
+		 * tem que ser igual a 2 e a linha tem que ser igual a 1 conforme explicação do
+		 * projeto.
+         */
+
+        boolean linhaDiferente = linha != vizinho.linha;
+        boolean colunaDiferente = coluna != vizinho.coluna;
+        boolean diagonal = linhaDiferente && colunaDiferente;
+
+        // Cálculo da linha e coluna
+        int deltaLinha = Math.abs(linha - vizinho.linha);
+        int deltaColuna = Math.abs(coluna - vizinho.coluna);
+        int deltaGeral = deltaLinha + deltaColuna;
+
+        // Lógia da linha e coluna
+        if (deltaGeral == 1 && !diagonal) {
+            vizinhos.add(vizinho);
+            return true;
+        } else if (deltaGeral == 2 && diagonal) {
+            vizinhos.add(vizinho);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Lógica de abrir o campo
+    void alterarMarcacao() {
+        if (!aberto) {
+            marcado = !marcado;
+        }
+    }
+
+    boolean abrir() {
+        if (!aberto && !marcado) {
+            aberto = true;
+
+            if (minado) {
+                // TODO Implementar nova versão
+            }
+
+            if (vizinhancaSegura()) {
+                // Passando um consumer
+                vizinhos.forEach(v -> v.abrir());
+            }
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    boolean vizinhancaSegura() {
+        // Passando um predicate
+        // Nenhum vizinho pode bater ou nenhum vizinho pode estar minado
+        return vizinhos.stream().noneMatch(v -> v.minado);
+    }
+
+    public boolean isMarcado() {
+        return marcado;
+    }
+
+    void minar() {
+        minado = true;
+    }
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+    void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public boolean isMinado() {
+        return minado;
+    }
+
+    /*
+	 * Como os atributos são constantes, ou seja, 'final', eles não tem como serem
+	 * modificados e devido a isso só se é permitido fazer o 'GET'.
+     */
+    public int getLinha() {
+        return linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    boolean objetivoAlcancado() {
+        boolean desvendado = !minado && aberto;
+        boolean protegido = minado && marcado;
+        return desvendado || protegido;
+    }
+
+    long minasNaVizinhanca() {
+        // Aqui ele vai filtrar todas as minas no campo e mostrar
+        return vizinhos.stream().filter(v -> v.minado).count();
+    }
+
+    void reiniciar() {
+        aberto = false;
+        minado = false;
+        marcado = false;
+    }
+}
---
+++ ProjetoCampoMinadoUDEMY-SWING/src/main/java/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -0,0 +1,87 @@
+package br.com.cod3r.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Tabuleiro {
+
+    private int quantLinhas;
+    private int quantColunas;
+    private int quantMinas;
+
+    private final List<Campo> campos = new ArrayList<>();
+
+    // Construtor
+    public Tabuleiro(int quantLinhasTabuleiro, int quantColunasTabuleiro, int quantMinasTabuleiro) {
+        this.quantLinhas = quantLinhasTabuleiro;
+        this.quantColunas = quantColunasTabuleiro;
+        this.quantMinas = quantMinasTabuleiro;
+
+        // Gerar campos através das linhas e colunas
+        gerarCampos();
+
+        // Gerar os vizinhos
+        associarVizinhos();
+
+        // Criação das minas
+        sortearMinas();
+    }
+
+    public void abrir(int linha, int coluna) {
+        try {
+            campos.parallelStream()
+                    .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                    .findFirst()
+                    .ifPresent(c -> c.abrir());
+        } catch (Exception e) {
+            //FIXME ajustar a implementação do método abrir
+            campos.forEach(c -> c.setAberto(true));
+            throw e;
+        }
+    }
+
+    public void alterarMarcacao(int linha, int coluna) {
+        campos.parallelStream()
+                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                .findFirst()
+                .ifPresent(c -> c.alterarMarcacao());
+    }
+
+    private void gerarCampos() {
+        for (int l = 0; l < quantLinhas; l++) {
+            for (int c = 0; c < quantColunas; c++) {
+                campos.add(new Campo(l, c));
+            }
+        }
+    }
+
+    private void associarVizinhos() {
+        for (Campo c1 : campos) {
+            for (Campo c2 : campos) {
+                c1.adicionarVizinho(c2);
+            }
+        }
+    }
+
+    private void sortearMinas() {
+        long minasArmadas = 0;
+
+        Predicate<Campo> minado = c -> c.isMinado();
+
+        do {
+            int aleatorio = (int) (Math.random() * campos.size());
+            campos.get(aleatorio).minar();
+            minasArmadas = campos.stream().filter(minado).count();
+        } while (minasArmadas < quantMinas);
+    }
+
+    public boolean objetivoAlcancado() {
+        return campos.stream().allMatch(c -> c.objetivoAlcancado());
+    }
+
+    public void reiniciar() {
+        campos.stream().forEach(c -> c.reiniciar());
+        sortearMinas();
+    }
+}
