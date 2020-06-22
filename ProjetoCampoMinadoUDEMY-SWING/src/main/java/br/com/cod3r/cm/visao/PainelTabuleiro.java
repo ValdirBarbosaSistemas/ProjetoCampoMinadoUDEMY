3f0b91b7b712bdad5eb576d9131792791b93b782
Valdir Barbosa da Silva Junior <valdir.barbosasistemas@gmail.com>
2020-06-22 11:47:25 -0300

    TESTE
    TESTE

---
+++ ProjetoCampoMinadoUDEMY-SWING/src/main/java/br/com/cod3r/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.cm.visao;
+
+import br.com.cod3r.cm.modelo.Tabuleiro;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+
+	// Construtor
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+
+	}
+}
---
+++ ProjetoCampoMinadoUDEMY-SWING/src/main/java/br/com/cod3r/cm/visao/TelaPrincipal.java
@@ -0,0 +1,37 @@
+package br.com.cod3r.cm.visao;
+
+import br.com.cod3r.cm.modelo.Tabuleiro;
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class TelaPrincipal extends JFrame {
+
+	// Construtor
+	public TelaPrincipal() {
+		Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);
+
+		// Adicionando o painel do próprio TABULEIRO
+		PainelTabuleiro painelTabuleiro = new PainelTabuleiro(tabuleiro);
+		add(painelTabuleiro);
+
+		setVisible(true);
+		setTitle("Campo Minado");
+		setSize(690, 438);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		new TelaPrincipal();
+	}
+
+	/*
+	 * CONFIGURAÇÕES DA TELA DO JOGO 
+	 * Visibilidade da tela 
+	 * Título da tela 
+	 * Tamanho da tela 
+	 * Localização da tela 
+	 * Fechamento da tela
+	 * 
+	 */
+}
